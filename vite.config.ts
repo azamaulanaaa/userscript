86d52323d18a215c03332651e43329461418b885
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-11 20:56:02 +0700

    feat: add import path alias @/ for ./src/

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { dirname, fromFileUrl, resolve } from "@std/path";
 import monkey from "vite-plugin-monkey";
+
+const __dirname = dirname(fromFileUrl(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     monkey({
       entry: "src/main.ts",
